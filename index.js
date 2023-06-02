7b11b8ef2e3edf100995765b69e19a2b9d6ba866
hasibur_rahman <hasiburuoda@gmail.com>
2023-06-02 18:09:21 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		client.connect();
 		const collections = client.db('toy_market').collection('toys');
 
 		// get all toys from database:
@@ -87,7 +87,7 @@ async function run() {
 
 
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
+		client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
 	} finally {
 		// Ensures that the client will close when you finish/error
